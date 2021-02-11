f9ca190a6a870f4a4a8d065b6d4de71b60cfdf00
mtj0928 <mtj0928@users.noreply.github.com>
2021-02-11 23:05:16 +0900

    fix access level

--- Sources/DeerKit/URL+extension.swift
+++ Sources/DeerKit/URL+extension.swift
@@ -9,11 +9,11 @@ import Foundation
 
 extension URL {
 
-    var queryKeys: [String] {
+    public var queryKeys: [String] {
         URLComponents(string: absoluteString)?.queryItems?.map { $0.name } ?? []
     }
 
-    func queryValue(for key: String) -> String? {
+    public func queryValue(for key: String) -> String? {
         let queryItems = URLComponents(string: absoluteString)?.queryItems
         return queryItems?.filter { $0.name == key }.compactMap { $0.value }.first
     }
