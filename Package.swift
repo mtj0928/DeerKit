9d1d601b72054f78e5d248c0343c9d694b516aaa
mtj0928 <mtj0928@users.noreply.github.com>
2024-09-05 08:45:06 +0900

    Minor updates

--- Package.swift
+++ Package.swift
@@ -10,20 +10,21 @@ let package = Package(
     platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v7), .visionOS(.v1), .tvOS(.v13)],
     products: [
         .library(name: "DeerKit", targets: [
+            "DeerFoundation",
             "DeerSwift",
             "DeerSwiftUI",
-            "DeerUserDefaults",
-            "DeerFoundation"
+            "DeerUserDefaults"
         ]),
+
+        .library(name: "DeerFoundation", targets: ["DeerFoundation"]),
         .library(name: "DeerSwift", targets: ["DeerSwift"]),
         .library(name: "DeerSwiftUI", targets: ["DeerSwiftUI"]),
         .library(name: "DeerUserDefaults", targets: ["DeerUserDefaults"]),
-        .library(name: "DeerFoundation", targets: ["DeerFoundation"]),
     ],
     targets: [
+        .target(name: "DeerFoundation", swiftSettings: swiftSettings),
         .target(name: "DeerSwift", swiftSettings: swiftSettings),
         .target(name: "DeerSwiftUI", swiftSettings: swiftSettings),
-        .target(name: "DeerFoundation", swiftSettings: swiftSettings),
         .target(name: "DeerUserDefaults", swiftSettings: swiftSettings),
 
         // Test
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DeerKit
-This library is @mtj0928's personal library.
+This is @mtj0928's personal usage.
+Feel free to use this package or copy the source code in this repository to your project.
 
 ## License
 MIT
